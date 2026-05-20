069aa208369958bad8320050fadba1e7a447c99d
ngolenghia <nghianl.24ns@vku.udn.vn>
2026-05-20 22:54:53 +0700

    Fix full static and csrf

--- config/settings.py
+++ config/settings.py
@@ -1,13 +1,11 @@
-"""
-Django settings for config project.
-"""
 import dj_database_url
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv() 
 
-# Build paths 
+load_dotenv()
+
+# Build paths
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
@@ -16,16 +14,16 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-fallback-key')
 # ĐỂ DEBUG = FALSE KHI CHẠY TRÊN AZURE
 DEBUG = False
 
+ALLOWED_HOSTS = [
+    'vku-timetable-lenghiagroup-f9gsdyaaaghmhzfy.southeastasia-01.azurewebsites.net',
+    '127.0.0.1',
+    'localhost'
+]
 
-ALLOWED_HOSTS = ['vku-timetable-lenghiagroup-f9gsdyaaaghmhzfy.southeastasia-01.azurewebsites.net', '127.0.0.1', 'localhost']
-
-# --- SỬA LỖI 403 TRÊN AZURE ---
-CSRF_TRUSTED_ORIGINS = ['https://vku-timetable-lenghiagroup-f9gsdyaaaghmhzfy.southeastasia-01.azurewebsites.net']
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME) 
-
+# SỬA LỖI 403 TRÊN AZURE
+CSRF_TRUSTED_ORIGINS = [
+    'https://vku-timetable-lenghiagroup-f9gsdyaaaghmhzfy.southeastasia-01.azurewebsites.net'
+]
 
 # Application definition
 INSTALLED_APPS = [
@@ -40,7 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Giúp phục vụ file giao diện
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,26 +83,23 @@ TIME_ZONE = 'Asia/Ho_Chi_Minh'
 USE_I18N = True
 USE_TZ = True
 
-
-# --- CẤU HÌNH FILE GIAO DIỆN (STATIC) ---
-STATIC_URL = 'static/'
+# FILE GIAO DIỆN (STATIC)
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# SỬA DÒNG NÀY ĐỂ HIỆN GIAO DIỆN ADMIN CHUẨN ĐÉT
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# --- EMAIL ---
+# EMAIL
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
-DEFAULT_FROM_EMAIL = 'Hệ thống Lịch học VKU <noreply@vku.udn.vn>'
 
-# --- ĐIỀU HƯỚNG ---
+# ĐIỀU HƯỚNG
 LOGIN_URL = 'login'
 LOGOUT_URL = 'login'
-LOGIN_REDIRECT_URL = 'home' 
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
